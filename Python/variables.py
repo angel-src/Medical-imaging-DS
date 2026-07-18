ca112d1f2e5298b109a4553844d9fcae40c1ec47
Angel <angel.git.in@gmail.com>
2026-07-18 12:58:58 +0530

    Create variables.py

---
+++ Python/variables.py
@@ -0,0 +1,15 @@
+age= 20
+print (age , type(age))
+
+score= 9.5
+print (score , type(score))
+
+name= "Angel"
+print (name , type(name))
+
+address= None
+print (address , type(address))
+
+is_tall= true
+print (is_tall , type(is_tall))
+
